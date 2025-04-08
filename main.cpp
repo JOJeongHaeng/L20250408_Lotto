5663563d1c2155a3c8e545a0bf0a890358919a86
JeongHaeng Cho <118785061+JOJeongHaeng@users.noreply.github.com>
2025-04-08 09:43:55 +0900

    채점 기능 추가

--- main.cpp
+++ main.cpp
@@ -38,6 +38,7 @@ void SetRandomNumber()
 	{
 		RandomNumber[Index] = Balls[Index];
 	}
+	sort(RandomNumber, RandomNumber + 6);
 }
 
 void Drawing()
@@ -45,6 +46,28 @@ void Drawing()
 	for (int Index = 0; Index < 6; Index++)
 	{
 		MyNumber[Index] = Balls[Index];
+	}
+	sort(MyNumber, MyNumber + 6);
+}
+
+void result()
+{
+	cout << endl;
+	int correct = 0;
+	for (int i = 0; i < 6; i++)
+	{
+		if (RandomNumber[i] == MyNumber[i])
+		{
+			correct += 1;
+		}
+	}
+	if (correct < 3)
+	{
+		cout << "¾Æ½±°Ôµµ ³«Ã· µÇ¼Ì½À´Ï´Ù.";
+	}
+	else 
+	{
+		cout << (7 - correct) << "%dµî ´çÃ·! ÃàÇÏµå¸³´Ï´Ù.";
 	}
 }
 
@@ -64,6 +87,7 @@ int main()
 	{
 		cout << MyNumber[i] << "\t";
 	}
+	result();
 
 	return 0;
 }
